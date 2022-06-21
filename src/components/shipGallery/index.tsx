8f0f4f39e663f8d7dc25f0e8bc3d1e41b99ca832
chnt <cihantklc@gmail.com>
2022-06-22 01:01:47 +0200

    - added ship gallery component

---
+++ src/components/shipGallery/index.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Ship } from '../../api/types';
+import ShipView, { View } from '../ship';
+
+export type ShipGalleryProps = {
+  loading?: boolean;
+  data?: Ship[];
+  view?: View;
+};
+
+export default function ShipGallery({
+  data,
+  loading,
+  view = View.GALLERY
+}: ShipGalleryProps) {
+  return (
+    <Box
+      maxWidth="xl"
+      data-testid="ShipGalleryContainer"
+      flexDirection={view === View.GALLERY ? 'row' : 'column'}
+      sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        gap: 2
+      }}
+    >
+      {loading ? (
+        <CircularProgress data-testid="ShipGallerySpinner" />
+      ) : (
+        data?.map((item) => <ShipView key={item.id} data={item} view={view} />)
+      )}
+    </Box>
+  );
+}
